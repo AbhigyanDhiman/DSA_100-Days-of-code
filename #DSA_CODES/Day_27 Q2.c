7793aaaeeb9e2d05450bb62d3287ec0733c9814c
AbhigyanDhiman <dhimanabhigyan7@gmail.com>
2026-02-27 19:46:57 +0530

    Day_27 Q2.c

---
+++ #DSA_CODES/Day_27 Q2.c
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    void removeLoop(Node* head) 
+    {
+        if(head == NULL || head->next == NULL)
+            return;
+
+        Node *slow = head, *fast = head;
+
+        while(fast != NULL && fast->next != NULL)
+        {
+            slow = slow->next;
+            fast = fast->next->next;
+
+            if(slow == fast)
+                break;
+        }
+
+        if(slow != fast)
+            return;
+
+        slow = head;
+
+        if(slow == fast)
+        {
+            while(fast->next != slow)
+                fast = fast->next;
+        }
+        else
+        {
+            while(slow->next != fast->next)
+            {
+                slow = slow->next;
+                fast = fast->next;
+            }
+        }
+
+        fast->next = NULL;
+    }
+};
