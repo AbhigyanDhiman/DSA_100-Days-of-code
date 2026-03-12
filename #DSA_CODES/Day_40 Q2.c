9ee6dd5ec01ccdddc75e8c41d7111fb0942144ec
AbhigyanDhiman <dhimanabhigyan7@gmail.com>
2026-03-12 22:05:31 +0530

    Day_40 Q2.c

---
+++ #DSA_CODES/Day_40 Q2.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+
+int* dailyTemperatures(int* temperatures, int temperaturesSize, int* returnSize) {
+    
+    int *result = (int*)calloc(temperaturesSize, sizeof(int));
+    int *stack = (int*)malloc(sizeof(int) * temperaturesSize);
+    int top = -1;
+
+    *returnSize = temperaturesSize;
+
+    for(int i = 0; i < temperaturesSize; i++)
+    {
+        while(top >= 0 && temperatures[i] > temperatures[stack[top]])
+        {
+            int index = stack[top--];
+            result[index] = i - index;
+        }
+
+        stack[++top] = i;
+    }
+
+    free(stack);
+    return result;
+}
