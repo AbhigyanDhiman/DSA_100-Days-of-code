ab24bba378bb165664afa9e7088d5015423c5da5
AbhigyanDhiman <dhimanabhigyan7@gmail.com>
2026-03-15 23:44:18 +0530

    Day_43 Q1.c

---
+++ #DSA_CODES/Day_43 Q1.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* left;
+    struct Node* right;
+};
+
+struct Node* createNode(int val) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = val;
+    newNode->left = NULL;
+    newNode->right = NULL;
+    return newNode;
+}
+
+void inorder(struct Node* root) {
+    if (root == NULL)
+        return;
+
+    inorder(root->left);
+    printf("%d ", root->data);
+    inorder(root->right);
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int arr[n];
+    for (int i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    if (arr[0] == -1)
+        return 0;
+
+    struct Node* nodes[n];
+
+    for (int i = 0; i < n; i++) {
+        if (arr[i] == -1)
+            nodes[i] = NULL;
+        else
+            nodes[i] = createNode(arr[i]);
+    }
+
+    for (int i = 0, j = 1; j < n; i++) {
+        if (nodes[i] != NULL) {
+            if (j < n)
+                nodes[i]->left = nodes[j++];
+            if (j < n)
+                nodes[i]->right = nodes[j++];
+        }
+    }
+
+    inorder(nodes[0]);
+
+    return 0;
+}
