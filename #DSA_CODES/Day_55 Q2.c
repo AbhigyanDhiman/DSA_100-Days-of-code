b8fe3a54af9ae06ffc5e430ef6ccadcfd99fc195
Abhigyan <dhimanabhigyan7@gmail.com>
2026-03-27 08:49:39 +0530

    Day_55 Q2.c

---
+++ #DSA_CODES/Day_55 Q2.c
@@ -0,0 +1,34 @@
+int* rightSideView(struct TreeNode* root, int* returnSize) {
+    if (!root) {
+        *returnSize = 0;
+        return NULL;
+    }
+
+    struct TreeNode* queue[1000];
+    int front = 0, rear = 0;
+
+    int* result = (int*)malloc(sizeof(int) * 1000);
+    int count = 0;
+
+    queue[rear++] = root;
+
+    while (front < rear) {
+        int size = rear - front;
+
+        for (int i = 0; i < size; i++) {
+            struct TreeNode* curr = queue[front++];
+
+            if (i == size - 1)
+                result[count++] = curr->val;
+
+            if (curr->left)
+                queue[rear++] = curr->left;
+
+            if (curr->right)
+                queue[rear++] = curr->right;
+        }
+    }
+
+    *returnSize = count;
+    return result;
+}
